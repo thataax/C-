7e094205f1437b2d8ec12f85b877ce59f2fd64f9
Thais Pereira de Oliveira <Thaispoliveira07@gmail.com>
2024-06-22 19:23:33 -0300

    Jogo da velha

---
+++ Jogo_da_velha/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace Jogo_da_velha
+{
+    partial class tela1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.lbl_3 = new System.Windows.Forms.Label();
+            this.lbl_5 = new System.Windows.Forms.Label();
+            this.lbl_7 = new System.Windows.Forms.Label();
+            this.lbl_4 = new System.Windows.Forms.Label();
+            this.lbl_6 = new System.Windows.Forms.Label();
+            this.lbl_8 = new System.Windows.Forms.Label();
+            this.lbl_9 = new System.Windows.Forms.Label();
+            this.var_turno = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.Location = new System.Drawing.Point(850, 646);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(202, 23);
+            this.btn_sair.TabIndex = 1;
+            this.btn_sair.Text = "cair fora daqui";
+            this.btn_sair.UseVisualStyleBackColor = true;
+            this.btn_sair.Click += new System.EventHandler(this.btn_sair_Click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(34, 79);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(63, 65);
+            this.btn_1.TabIndex = 2;
+            this.btn_1.Text = "button1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 65);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(211, 253);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 65);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(124, 253);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(63, 65);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(124, 168);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(63, 65);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(124, 89);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(63, 65);
+            this.btn_2.TabIndex = 7;
+            this.btn_2.Text = "button6";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(34, 253);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 65);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(34, 168);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(63, 65);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(211, 89);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(63, 65);
+            this.btn_3.TabIndex = 10;
+            this.btn_3.Text = "button9";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.btn_3);
+            this.groupBox1.Controls.Add(this.btn_1);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.btn_2);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Location = new System.Drawing.Point(833, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 395);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_1.Location = new System.Drawing.Point(426, 242);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(49, 39);
+            this.lbl_1.TabIndex = 12;
+            this.lbl_1.Text = "[ ]";
+            this.lbl_1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_2.Location = new System.Drawing.Point(505, 242);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(49, 39);
+            this.lbl_2.TabIndex = 13;
+            this.lbl_2.Text = "[ ]";
+            // 
+            // lbl_3
+            // 
+            this.lbl_3.AutoSize = true;
+            this.lbl_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_3.Location = new System.Drawing.Point(594, 252);
+            this.lbl_3.Name = "lbl_3";
+            this.lbl_3.Size = new System.Drawing.Size(49, 39);
+            this.lbl_3.TabIndex = 14;
+            this.lbl_3.Text = "[ ]";
+            // 
+            // lbl_5
+            // 
+            this.lbl_5.AutoSize = true;
+            this.lbl_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_5.Location = new System.Drawing.Point(505, 331);
+            this.lbl_5.Name = "lbl_5";
+            this.lbl_5.Size = new System.Drawing.Size(49, 39);
+            this.lbl_5.TabIndex = 15;
+            this.lbl_5.Text = "[ ]";
+            // 
+            // lbl_7
+            // 
+            this.lbl_7.AutoSize = true;
+            this.lbl_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_7.Location = new System.Drawing.Point(426, 395);
+            this.lbl_7.Name = "lbl_7";
+            this.lbl_7.Size = new System.Drawing.Size(49, 39);
+            this.lbl_7.TabIndex = 16;
+            this.lbl_7.Text = "[ ]";
+            // 
+            // lbl_4
+            // 
+            this.lbl_4.AutoSize = true;
+            this.lbl_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_4.Location = new System.Drawing.Point(426, 321);
+            this.lbl_4.Name = "lbl_4";
+            this.lbl_4.Size = new System.Drawing.Size(49, 39);
+            this.lbl_4.TabIndex = 17;
+            this.lbl_4.Text = "[ ]";
+            // 
+            // lbl_6
+            // 
+            this.lbl_6.AutoSize = true;
+            this.lbl_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_6.Location = new System.Drawing.Point(584, 321);
+            this.lbl_6.Name = "lbl_6";
+            this.lbl_6.Size = new System.Drawing.Size(49, 39);
+            this.lbl_6.TabIndex = 18;
+            this.lbl_6.Text = "[ ]";
+            // 
+            // lbl_8
+            // 
+            this.lbl_8.AutoSize = true;
+            this.lbl_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_8.Location = new System.Drawing.Point(505, 395);
+            this.lbl_8.Name = "lbl_8";
+            this.lbl_8.Size = new System.Drawing.Size(49, 39);
+            this.lbl_8.TabIndex = 19;
+            this.lbl_8.Text = "[ ]";
+            // 
+            // lbl_9
+            // 
+            this.lbl_9.AutoSize = true;
+            this.lbl_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_9.Location = new System.Drawing.Point(584, 395);
+            this.lbl_9.Name = "lbl_9";
+            this.lbl_9.Size = new System.Drawing.Size(49, 39);
+            this.lbl_9.TabIndex = 20;
+            this.lbl_9.Text = "[ ]";
+            // 
+            // var_turno
+            // 
+            this.var_turno.AutoSize = true;
+            this.var_turno.Location = new System.Drawing.Point(901, 63);
+            this.var_turno.Name = "var_turno";
+            this.var_turno.Size = new System.Drawing.Size(13, 13);
+            this.var_turno.TabIndex = 21;
+            this.var_turno.Text = "1";
+            this.var_turno.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // tela1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1850, 681);
+            this.Controls.Add(this.var_turno);
+            this.Controls.Add(this.lbl_9);
+            this.Controls.Add(this.lbl_8);
+            this.Controls.Add(this.lbl_6);
+            this.Controls.Add(this.lbl_4);
+            this.Controls.Add(this.lbl_7);
+            this.Controls.Add(this.lbl_5);
+            this.Controls.Add(this.lbl_3);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.lbl_1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_sair);
+            this.MaximumSize = new System.Drawing.Size(30000, 75000);
+            this.MinimumSize = new System.Drawing.Size(1080, 720);
+            this.Name = "tela1";
+            this.Text = "Jogo da velha";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_sair;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.Label lbl_3;
+        private System.Windows.Forms.Label lbl_5;
+        private System.Windows.Forms.Label lbl_7;
+        private System.Windows.Forms.Label lbl_4;
+        private System.Windows.Forms.Label lbl_6;
+        private System.Windows.Forms.Label lbl_8;
+        private System.Windows.Forms.Label lbl_9;
+        private System.Windows.Forms.Label var_turno;
+    }
+}
+
---
+++ Jogo_da_velha/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_da_velha
+{
+    public partial class tela1 : Form
+    {
+        public tela1()
+        {
+            InitializeComponent();
+        }
+
+        public void Redirect(string url);
+        {
+
+
+        }
+
+        private string Definir_turno()
+        {
+            if (var_turno.Text == "1")
+            {
+                return "x";
+            }
+            else
+            {
+                return "O";
+            }
+
+        }
+
+        private void btn_sair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            if (lbl_1.Text == "[ ]"){
+
+                lbl_1.Text = Definir_turno();
+                var_turno.Text = Convert.ToString(Convert.ToInt32(var_turno.Text) * -1);
+
+            }
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            if (lbl_2.Text == "[ ]")
+            {
+
+                lbl_2.Text = Definir_turno();
+                var_turno.Text = Convert.ToString(Convert.ToInt32(var_turno.Text) * -1);
+
+            }
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            if (lbl_3.Text == "[ ]")
+            {
+
+                lbl_3.Text = Definir_turno();
+                var_turno.Text = Convert.ToString(Convert.ToInt32(var_turno.Text) * -1);
+
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (lbl_4.Text == "[ ]")
+            {
+
+                lbl_4.Text = Definir_turno();
+                var_turno.Text = Convert.ToString(Convert.ToInt32(var_turno.Text) * -1);
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (lbl_5.Text == "[ ]")
+            {
+
+                lbl_5.Text = Definir_turno();
+                var_turno.Text = Convert.ToString(Convert.ToInt32(var_turno.Text) * -1);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (lbl_6.Text == "[ ]")
+            {
+
+                lbl_6.Text = Definir_turno();
+                var_turno.Text = Convert.ToString(Convert.ToInt32(var_turno.Text) * -1);
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (lbl_7.Text == "[ ]")
+            {
+
+                lbl_7.Text = Definir_turno();
+                var_turno.Text = Convert.ToString(Convert.ToInt32(var_turno.Text) * -1);
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (lbl_8.Text == "[ ]")
+            {
+
+                lbl_8.Text = Definir_turno();
+                var_turno.Text = Convert.ToString(Convert.ToInt32(var_turno.Text) * -1);
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (lbl_9.Text == "[ ]")
+            {
+
+                lbl_9.Text = Definir_turno();
+                var_turno.Text = Convert.ToString(Convert.ToInt32(var_turno.Text) * -1);
+
+            }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
